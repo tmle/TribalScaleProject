881065ca7b3e19d2a7e69ff9b30dfc9d731a13e9
Thinh M. Le <thinh.m.le@gmail.com>
2017-06-03 13:29:34 -0700

    Initial Commit

---
+++ TribalScale/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TribalScale
+//
+//  Created by Thinh Le on 2017-06-03.
+//  Copyright © 2017 Lac Viet Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TribalScale/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TribalScale
+//
+//  Created by Thinh Le on 2017-06-03.
+//  Copyright © 2017 Lac Viet Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
